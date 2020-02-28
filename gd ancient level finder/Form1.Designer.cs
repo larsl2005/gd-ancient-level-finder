daa4edd015433ba7694b2472ac8c80442ae4a9f2
larsl2005 <32791312+larsl2005@users.noreply.github.com>
2020-02-28 13:58:08 +0100

    ay

---
+++ gd ancient level finder/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace gd_ancient_level_finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(277, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            129,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Max id";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(394, 281);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Min id";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(54, 30);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 389);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GD level thing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ gd ancient level finder/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gd_ancient_level_finder
+{
+    
+    public partial class Form1 : Form
+    {
+        public int FFound = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int max = Convert.ToInt32(numericUpDown1.Value);
+            int min = Convert.ToInt32(numericUpDown2.Value);
+            FFound = 0;
+            for(int times = min; times < max + 1; times++) //repeats until max id is reached
+            {
+                 HttpClient client = new HttpClient();
+        var values = new Dictionary<string, string>
+            {
+            //all arguments it sends to the server
+                { "gameVersion", "21" }, //game version, the 21 means 2.1, 20 means 2.0, 22 means 2.2, ect
+                { "binaryVersion", "35" },//honestly i have no idea what this means
+                { "gdw", "0" }, //i think this means if it is gd world or not (i will double check later)
+                { "accountID", "71" }, // rubrub's acc id
+                { "type", "0" }, //type, this means if its a search, recent, magic, trending, ect.
+                { "str", times.ToString() },// this is the level id
+                { "diff", "-" }, // i think this means diffeculty
+                { "len", "-" },// idk what this means
+                { "page", "0" }, //this means page
+                { "total", "0" }, //idk what this means
+                { "uncompleted", "0" }, //search filter
+                { "onlyCompleted", "0" },//search filter
+                { "featured", "0" },//search filter
+                { "original", "0" },//search filter
+                { "twoPlayer", "0" },//search filter
+                { "coins", "0" },//search filter
+                { "epic", "0" },//search filter or mod send filter
+                { "local", "1" },//idk
+                { "secret", "Wmfd2893gb7" }//robtop's way of security: a code thats always the same
+            };
+                var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("http://www.boomlings.com/database/getGJLevels21.php", content); // sends everything to the server
+
+            var responseString = await response.Content.ReadAsStringAsync();
+                if(times == max + 1)
+                {
+                    if (times == max)
+                    {
+                        Console.WriteLine("Done"); // if everything is done, it prints done in the output
+                        Console.WriteLine("Program has found " + FFound + " levels."); // writes the amount of levels it found
+                    }
+                }
+                if(responseString == "-1") //checks if the server responded -1, wich is robtops way of saying fuck off
+                {
+                    Console.WriteLine("Level ID " +times+ " either does not exist , the servers are down or you are IP banned. Server responded: " +responseString+"");
+                }
+                else // if the level was found, it does this
+                {
+                    FFound = FFound + 1; // adds to amount of levels it found
+                    //string[] eeee = { ":" };
+                    //Int32 count = times; // some unused code that doesnt work
+                    //String help = responseString.Split(eeee, times);
+                    richTextBox1.Text = richTextBox1.Text + " " +
+                        "";//idk why i made this line so advanced its literally adding a space
+                    richTextBox1.Text = richTextBox1.Text + times.ToString(); // prints it in the textbox
+                    Console.WriteLine("Level ID " + times + " found, server responded: " +responseString+ ""); // prints it to the output
+                
+                }
+                
+            
+        }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Title = "OUTPUT";
+        }
+    }
+}
